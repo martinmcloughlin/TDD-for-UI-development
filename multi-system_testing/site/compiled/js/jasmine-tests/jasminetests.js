d2a5c259d3959a2c98ce4461af8e1db5f8afdbe3
Martin McLoughlin <martin.mcloughlin@spindriftgroup.com>
2014-10-07 16:51:14 +0100

    include coverage reporting

--- multi-system_testing/site/compiled/js/jasmine-tests/jasminetests.js
+++ multi-system_testing/site/compiled/js/jasmine-tests/jasminetests.js
@@ -1,17 +1,16 @@
-describe("simpleTest02", function() {
-    it("make sure today is not the 19th", function() {
-        //expect(nd).toBe(nd); // pass
-        expect(nd).not.toBe(19); // fail
-    });
-});
-
 describe("simpleTest01", function() {
     it("make sure today is not null", function() {
         expect(d).not.toBe(null); // pass
-        //expect(d).toBe(null); // fail
     });
+});
+describe("simpleTest02", function() {
+    it("make sure today is not the 19th", function() {
+        expect(nd).not.toBe(19); // fail
+    });
+});
+describe("simpleTest03", function() {
     it("spots mmx", function() {
         expect(mmx).not.toBe(null);
-    })
+    });
 });
 
--- multi-system_testing/site/test-scripts/jasmine/simpleJasTwo.js
+++
@@ -1,6 +0,0 @@
-describe("simpleTest02", function() {
-    it("make sure today is not the 19th", function() {
-        //expect(nd).toBe(nd); // pass
-        expect(nd).not.toBe(19); // fail
-    });
-});
